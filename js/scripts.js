a3282e9b2ef27b2db9a23f2320026873d3e54abb
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-02-14 17:24:29 +0300

    final commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
 
 function akanName(){
 
-    var year = document.getElementById("bdyear").value;
-    var CC = parseInt(year.slice(0,2));
-    var YY = parseInt(year.slice(2,4));
-    var MM = parseInt(document.getElementById("bdmonth").value) ;
-    var DD = parseInt(document.getElementById("bddate").value);
+    let year = document.getElementById("bdyear").value;
+    let CC = parseInt(year.slice(0,2));
+    let YY = parseInt(year.slice(2,4));
+    let MM = parseInt(document.getElementById("bdmonth").value) ;
+    let DD = parseInt(document.getElementById("bddate").value);
     if (DD <= 0 || DD>31 ||MM <= 0 || MM>12){
         alert("Date or Month is invalid,\nDates must be between 1 and 31\nMonths must be between 1 and 12 ");
         return;
         
     }
-    var gender = document.getElementById("gender").value;
-    var male=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
-    var female=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
-    var days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    var dayOfTheWeek=parseInt((( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7));
+    let gender = document.getElementById("gender").value;
+    let male=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    let female=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    let days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let dayOfTheWeek=parseInt((( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7));
     if (gender === "female"){
 
         document.getElementById("day-born").innerText=("You were born on "+days[dayOfTheWeek]);
