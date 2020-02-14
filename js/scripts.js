e3700579f07430afc034f2ba4f0e2e97ca3a67cc
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-02-14 14:26:32 +0300

    created days  array

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,7 @@ function akanName(){
     var gender = document.getElementById("gender").value;
     var male=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     var female=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
+    var days=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
    
 
