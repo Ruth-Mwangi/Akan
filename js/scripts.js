c7a1890638448f3c2c98a5a1691a7bc02eb153a0
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-02-14 14:23:37 +0300

    created if statement to check date and month input is valid

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,11 @@ function akanName(){
     var YY = parseInt(year.slice(2,4));
     var MM = parseInt(document.getElementById("bdmonth").value) ;
     var DD = parseInt(document.getElementById("bddate").value);
+    if (DD <= 0 || DD>31 ||MM <= 0 || MM>12){
+        alert("Date or Month is invalid,\nDates must be between 1 and 31\nMonths must be between 1 and 12 ");
+        return;
+        
+    }
    
 
 
