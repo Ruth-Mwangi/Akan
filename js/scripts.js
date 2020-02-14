4595efc379a8019828409d160ae8703c9f6177f4
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-02-14 14:25:51 +0300

    created female names array

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,7 @@ function akanName(){
     }
     var gender = document.getElementById("gender").value;
     var male=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
+    var female=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
 
    
 
