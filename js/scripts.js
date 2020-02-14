fcbb67a43dc961ba16e1729e233a3ce33f658bff
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-02-14 14:22:31 +0300

    captured date details from form

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ function akanName(){
     var CC = parseInt(year.slice(0,2));
     var YY = parseInt(year.slice(2,4));
     var MM = parseInt(document.getElementById("bdmonth").value) ;
+    var DD = parseInt(document.getElementById("bddate").value);
    
 
 
