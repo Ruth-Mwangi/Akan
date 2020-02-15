a271a7b124dd5ff89c53a83a2fe0252ebac55d6a
Ruth-Mwangi <rwmwangi96@gmail.com>
2020-02-15 10:07:44 +0300

    final commit

--- js/scripts.js
+++ js/scripts.js
@@ -6,11 +6,22 @@ function akanName(){
     let YY = parseInt(year.slice(2,4));
     let MM = parseInt(document.getElementById("bdmonth").value) ;
     let DD = parseInt(document.getElementById("bddate").value);
-    if (DD <= 0 || DD>31 ||MM <= 0 || MM>12){
-        alert("Date or Month is invalid,\nDates must be between 1 and 31\nMonths must be between 1 and 12 ");
-        return;
-        
+
+    if (DD <= 0 || DD>31){
+        document.getElementById('bddate').value = '';
+        alert("Date is invalid,\nDates must be between 1 and 31 ");
+        return;   
     }
+    if (MM <= 0 || MM>12){
+        document.getElementById('bdmonth').value = '';
+        alert("Month is invalid,\nMonths must be between 1 and 12 ");
+        return;   
+    }
+    if(MM === ""||DD===""||year===""){
+        alert("Please fill in all fields.");
+        return;
+    }
+
     let gender = document.getElementById("gender").value;
     let male=["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"];
     let female=["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"];
@@ -18,14 +29,15 @@ function akanName(){
     let dayOfTheWeek=parseInt((( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7));
     if (gender === "female"){
 
-        document.getElementById("day-born").innerText=("You were born on "+days[dayOfTheWeek]);
-        document.getElementById("name").innerText=("Your Akan name is "+ female[dayOfTheWeek]);
+        document.getElementById("day-born").innerHTML=("You were born on "+days[dayOfTheWeek].toUpperCase());
+        document.getElementById("name").innerHTML=('<i class="fas fa-venus    "></i>'+" Your Akan name is "+ female[dayOfTheWeek]+" "+'<i class="fas fa-venus    "></i>');
 
     }
     else{
-        document.getElementById("day-born").innerText=("You were born on "+days[dayOfTheWeek]);
-        document.getElementById("name").innerText=("Your Akan name is "+ male[dayOfTheWeek]);
+        document.getElementById("day-born").innerHTML=("You were born on "+days[dayOfTheWeek].toUpperCase());
+        document.getElementById("name").innerHTML=('<i class="fas fa-mars    "></i>'+ " Your Akan name is "+ male[dayOfTheWeek]+" "+'<i class="fas fa-mars   "></i>');
     }
+    document.getElementById("myForm").reset();
 
 
 }
